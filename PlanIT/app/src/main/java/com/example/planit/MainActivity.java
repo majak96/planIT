7fffd1cb754013f965fdc17bd90a178b8c267710
majak96 <majak96@gmail.com>
2020-05-04 02:58:19 +0200

    Navigation drawer
    Added the navigation drawer. Updated the main activity to support the new toolbar and navigation drawer. Added a few icons for the navigation drawer. Added an example for fragments. Updated colors.xml file. Updated build.gradle with dependencies for Android Material and CircleImageView.

--- PlanIT/app/src/main/java/com/example/planit/MainActivity.java
+++ PlanIT/app/src/main/java/com/example/planit/MainActivity.java
@@ -1,14 +1,89 @@
 package com.example.planit;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.planit.fragments.CalendarFragment;
+import com.example.planit.fragments.TeamsFragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //if we enter the activity for the first time (not after rotating etc)
+        if(savedInstanceState == null) {
+            //TODO: change the default fragment?
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new CalendarFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_calendar);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_calendar:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CalendarFragment()).commit();
+                break;
+            case R.id.nav_habits:
+                Toast.makeText(this, "Habits!", Toast.LENGTH_SHORT).show();
+                //TODO: delete this and add habits fragment
+                break;
+            case R.id.nav_teams:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TeamsFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "Settings!", Toast.LENGTH_SHORT).show();
+                //TODO: delete this and add settings
+                break;
+            case R.id.nav_signout:
+                Toast.makeText(this, "Sign Out!", Toast.LENGTH_SHORT).show();
+                //TODO: delete this and implement sign out
+                break;
+            default:
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
