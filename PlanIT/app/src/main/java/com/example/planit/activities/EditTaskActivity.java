cd2ccf3252caf7ede5c9880bccfb07d3d7d9e7dc
majak96 <majak96@gmail.com>
2020-05-12 00:25:52 +0200

    Date and time for a new task
    Updated EditTaskActivity and activity_edittask.xml to add date and time for the task. Added DateDialogFragment.java.

--- PlanIT/app/src/main/java/com/example/planit/activities/EditTaskActivity.java
+++ PlanIT/app/src/main/java/com/example/planit/activities/EditTaskActivity.java
@@ -1,11 +1,14 @@
 package com.example.planit.activities;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -19,6 +22,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.planit.R;
 import com.example.planit.adapters.EditTaskAdapter;
+import com.example.planit.fragments.DateDialogFragment;
 import com.example.planit.fragments.LabelDialogFragment;
 import com.example.planit.fragments.PriorityDialogFragment;
 import com.example.planit.fragments.TaskReminderDialogFragment;
@@ -28,15 +32,18 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import model.Label;
 
-public class EditTaskActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, PriorityDialogFragment.PriorityDialogListener, LabelDialogFragment.LabelDialogListener {
+public class EditTaskActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener, PriorityDialogFragment.PriorityDialogListener, LabelDialogFragment.LabelDialogListener {
 
     private int selectedPriority;
     private EditTaskAdapter adapter;
     private TextView reminderTextView;
+    private TextView dateTextView;
+    private TextView timeTextView;
     private TextView priorityTextView;
     private List<Label> labels = new ArrayList<Label>();
 
@@ -51,7 +58,15 @@ public class EditTaskActivity extends AppCompatActivity implements TimePickerDia
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         reminderTextView = findViewById(R.id.reminder_create_task);
-        priorityTextView = findViewById((R.id.priority_create_task));
+        priorityTextView = findViewById(R.id.priority_create_task);
+        dateTextView = findViewById(R.id.date_create_task);
+        timeTextView = findViewById(R.id.time_create_task);
+
+        //set today's date
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("E, MMMM dd, YYYY");
+        String dateString = dateFormat.format(date);
+        dateTextView.setText(dateString);
 
         //set X instead of <-
         this.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
@@ -90,6 +105,41 @@ public class EditTaskActivity extends AppCompatActivity implements TimePickerDia
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    public void addDate(View v) {
+        DialogFragment dateDialogFragment = DateDialogFragment.getInstance();
+        dateDialogFragment.setCancelable(true);
+
+        dateDialogFragment.show(getSupportFragmentManager(), "Task date dialog");
+    }
+
+    public void addTime(View v) {
+        Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        TimePickerDialog dialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(Calendar.HOUR_OF_DAY, hour);
+                        calendar.set(Calendar.MINUTE, minute);
+
+                        DateFormat timeFormat = new SimpleDateFormat("HH:mm");
+                        String timeString = timeFormat.format(calendar.getTime());
+
+                        timeTextView.setText(timeString);
+                        timeTextView.setTextColor(getResources().getColor(R.color.darkGray));
+
+                        ImageButton removeImageView = findViewById(R.id.time_remove);
+                        removeImageView.setVisibility(View.VISIBLE);
+                    }
+                },
+                hour, minute, android.text.format.DateFormat.is24HourFormat(this));
+
+        dialog.show();
     }
 
     public void addReminder(View v) {
@@ -133,6 +183,19 @@ public class EditTaskActivity extends AppCompatActivity implements TimePickerDia
         removeImageView.setVisibility(View.VISIBLE);
     }
 
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+        DateFormat dateFormat = new SimpleDateFormat("E, MMMM dd, YYYY");
+        String dateString = dateFormat.format(calendar.getTime());
+
+        dateTextView.setText(dateString);
+    }
+
+
     @Override
     public void setPriority(String[] choices, Integer position) {
         String choice = choices[position];
@@ -159,9 +222,17 @@ public class EditTaskActivity extends AppCompatActivity implements TimePickerDia
         adapter.notifyItemInserted(labels.size() - 1);
     }
 
+    //remove the time
+    public void clearTime(View view) {
+        timeTextView.setText(R.string.add_time);
+        timeTextView.setTextColor(getResources().getColor(R.color.gray));
+
+        view.setVisibility(View.GONE);
+    }
+
     //remove the reminder
     public void clearReminder(View view) {
-        TextView reminderTextView = (TextView) findViewById(R.id.reminder_create_task);
+        reminderTextView = (TextView) findViewById(R.id.reminder_create_task);
         reminderTextView.setText(R.string.add_reminder);
         reminderTextView.setTextColor(getResources().getColor(R.color.gray));
 
@@ -170,7 +241,7 @@ public class EditTaskActivity extends AppCompatActivity implements TimePickerDia
 
     //remove the priority
     public void clearPriority(View view) {
-        TextView priorityTextView = (TextView) findViewById(R.id.priority_create_task);
+        priorityTextView = (TextView) findViewById(R.id.priority_create_task);
         priorityTextView.setText(R.string.add_priority);
         priorityTextView.setTextColor(getResources().getColor(R.color.gray));
 
