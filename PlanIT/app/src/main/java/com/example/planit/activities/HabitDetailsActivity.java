ce889a1302edbe008335535e922edb64661c9be4
vesnamilic <44587403+vesnamilic@users.noreply.github.com>
2020-05-12 01:35:26 +0200

    Updated HabitDetailsActivity
    Small changes

--- PlanIT/app/src/main/java/com/example/planit/activities/HabitDetailsActivity.java
+++ PlanIT/app/src/main/java/com/example/planit/activities/HabitDetailsActivity.java
@@ -1,7 +1,6 @@
 package com.example.planit.activities;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -25,29 +24,37 @@ public class HabitDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_habit_details);
+        this.setContentView(R.layout.activity_habit_details);
+
+        // toolbar settings
         Toolbar toolbar = findViewById(R.id.toolbar_habit);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        this.setSupportActionBar(toolbar);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        titleView = findViewById(R.id.habits_overview_recycle_view_name);
-        descriptionView = findViewById(R.id.habit_details_description);
-        habitNumberOfDaysTextView = findViewById(R.id.habits_overview_recycle_view_num_days);
-        switchDone = findViewById(R.id.habit_done);
+        this.titleView = findViewById(R.id.habits_overview_recycle_view_name);
+        this.descriptionView = findViewById(R.id.habit_details_description);
+        this.habitNumberOfDaysTextView = findViewById(R.id.habits_overview_recycle_view_num_days);
+        this.switchDone = findViewById(R.id.habit_done);
 
+        // check if habit exists
         if(getIntent().hasExtra("Habit")){
-            habit = (Habit) getIntent().getSerializableExtra("Habit");
+            this.habit = (Habit) getIntent().getSerializableExtra("Habit");
             this.setTitle(habit.getTitle());
-            titleView.setText(habit.getTitle());
-            descriptionView.setText(habit.getDescription());
-            habitNumberOfDaysTextView.setText(habit.getNumberOfDays().toString());
+            this.titleView.setText(habit.getTitle());
+            this.descriptionView.setText(habit.getDescription());
+            this.habitNumberOfDaysTextView.setText(habit.getNumberOfDays().toString());
         }
 
+        // switch on checked listener
         this.switchDone.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Log.v("Aaf","faSFAsf");
-                habit.setNumberOfDays(habit.getNumberOfDays() + 1);
+                if(isChecked) {
+                    habit.setNumberOfDays(habit.getNumberOfDays() + 1);
+                } else {
+                    habit.setNumberOfDays(habit.getNumberOfDays() - 1);
+                }
+                habitNumberOfDaysTextView.setText(habit.getNumberOfDays().toString());
             }
         });
     }
