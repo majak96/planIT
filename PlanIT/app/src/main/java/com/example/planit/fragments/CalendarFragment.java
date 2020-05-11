9c9e64cd062f3e487514f9e433f1825ad68bc7db
majak96 <majak96@gmail.com>
2020-05-11 21:58:59 +0200

    Team Calendar
    Updated CalendarFragment to show team calendar (monthly preview).  Added toolbar with menu items for team chat and team details preview for the team monthly preview. Updated AndroidManifest.xml.

--- PlanIT/app/src/main/java/com/example/planit/fragments/CalendarFragment.java
+++ PlanIT/app/src/main/java/com/example/planit/fragments/CalendarFragment.java
@@ -1,8 +1,11 @@
 package com.example.planit.fragments;
 
 import com.example.planit.R;
+import com.example.planit.activities.ChatActivity;
 import com.example.planit.activities.EditTaskActivity;
 import com.example.planit.activities.SettingsActivity;
+import com.example.planit.activities.TeamDetailActivity;
+import com.example.planit.mokaps.Mokap;
 import com.example.planit.utils.EventDecorator;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
@@ -12,9 +15,14 @@ import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -32,14 +40,27 @@ import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import model.Team;
+
 public class CalendarFragment extends Fragment {
 
     private static final String TAG = "CalendarFragment";
 
     ArrayList<CalendarDay> eventDates = new ArrayList<CalendarDay>();
 
-    public static CalendarFragment newInstance() {
-        return new CalendarFragment();
+    public static CalendarFragment newInstance(Long team) {
+        CalendarFragment fragment = new CalendarFragment();
+
+        if (team != null) {
+            Bundle args = new Bundle();
+            args.putLong("SELECTED_TEAM", team);
+
+            Log.d(TAG, "newInstance: " + team);
+
+            fragment.setArguments(args);
+        }
+
+        return fragment;
     }
 
     @Nullable
@@ -48,7 +69,18 @@ public class CalendarFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
 
         //set activity title
-        getActivity().setTitle(R.string.personal);
+        if (getArguments() != null) {
+
+            Long teamId = getArguments().getLong("SELECTED_TEAM");
+            Team team = Mokap.getTeam(teamId);
+
+            getActivity().setTitle(team.getName());
+
+            setHasOptionsMenu(true);
+        } else {
+            getActivity().setTitle(R.string.personal);
+
+        }
 
         //initializing a few example events
         initializeExampleEvents();
@@ -98,6 +130,42 @@ public class CalendarFragment extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.team_calendar_menu, menu);
+
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.menu_chat:
+                intent = new Intent(getActivity(), ChatActivity.class);
+
+                if (getArguments() != null) {
+                    Long teamId = getArguments().getLong("SELECTED_TEAM");
+                    intent.putExtra("team", teamId);
+                }
+
+                this.startActivity(intent);
+                break;
+            case R.id.menu_team_details:
+                intent = new Intent(getActivity(), TeamDetailActivity.class);
+
+                if (getArguments() != null) {
+                    Long teamId = getArguments().getLong("SELECTED_TEAM");
+                    intent.putExtra("team", teamId);
+                }
+
+                this.startActivity(intent);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     //TODO: delete this and add actual events
